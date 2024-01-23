bd173ea921142908d3b73a8cbb60e7524b83b80f
Festus1914 <oladiplentyfestus@gmail.com>
2024-01-23 21:55:09 +0100

    Adding sanity studio

---
+++ src/client.js
@@ -0,0 +1,6 @@
+import  { SanityClient } from "@sanity/client";
+
+export default SanityClient({
+    projectId: "e0rsdob2",
+    dataset: "production"
+})
---
+++ studio/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'e0rsdob2',
+    dataset: 'production'
+  }
+})
